6cb3f799fdecb9439d7ef6065f447ddd0758d349
David Abrahams <david.abrahams@students.olin.edu>
2018-01-31 18:33:46 -0500

    add simple constructor

--- datastructures/day02/src/your_code/MyLinkedList.java
+++ datastructures/day02/src/your_code/MyLinkedList.java
@@ -16,6 +16,12 @@ public class MyLinkedList {
             this.prev = prev;
             this.next = next;
         }
+
+        private Node(Chicken d) {
+            this.val = d;
+            prev = null;
+            next = null;
+        }
     }
 
     public MyLinkedList() {
