5409afb5d4436a6bc183f428c70bbe0d1836742b
davidabrahams <david.abrahams@students.olin.edu>
2018-03-28 13:20:36 -0400

    oops. did not mean to include this

--- graphs/day01/src/Permutations.java
+++ graphs/day01/src/Permutations.java
@@ -4,18 +4,6 @@ import java.util.Set;
 
 public class Permutations {
 
-    private static void backtrack(LinkedList<Integer> curr, Set<Integer> unused, List<List<Integer>> subsets) {
-        if (unused.isEmpty())
-            subsets.add(new LinkedList<>(curr));
-        for (int u : new LinkedList<>(unused)) {
-            curr.addLast(u);
-            unused.remove(u);
-            backtrack(curr, unused, subsets);
-            unused.add(u);
-            curr.removeLast();
-        }
-    }
-
     public static List<List<Integer>> permutations(List<Integer> A) {
         // TODO
         List<List<Integer>> permutations = new LinkedList<>();
